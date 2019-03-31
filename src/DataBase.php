941c54f18428fcd962b5f3f99eaa64e89131382e
Lukas Tassanyi <lukas.tassanyi@iota.org>
2019-03-31 10:57:54 +0200

    db: create and authenticate account

--- src/DataBase.php
+++ src/DataBase.php
@@ -13,6 +13,28 @@ class Database {
         incl_rel_once("mysql.php");
         $this->mysqli = connect_to_db();
     }
-}
 
-$db = new Database();
+    private function query($query) {
+        return $this->mysqli->query($query);
+    }
+
+    private function get_row($query) {
+        try {
+            $res = $this->query($query);
+            $row = $res->fetch_object();
+            return $row ? get_object_vars($row) : null;
+        } finally {
+            $res->close();
+        }
+    }
+
+    public function create_account($discord_id, $password) {
+        $pw_bcrypt = password_hash($password, PASSWORD_BCRYPT);
+        $this->query("INSERT INTO account (discord_id, pw_bcrypt) VALUES ('$discord_id', '$pw_bcrypt')");
+    }
+
+    public function authenticate_account($discord_id, $password) {
+        $account = $this->get_row("SELECT pw_bcrypt FROM account WHERE discord_id = '$discord_id'");
+        return $account !== null && password_verify($password, $account['pw_bcrypt']);
+    }
+}
